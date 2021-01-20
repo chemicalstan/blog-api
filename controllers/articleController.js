38e0fee94fcc37edf0624a796598e65da87c0ca9
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-21 00:55:03 +0100

    Expose update and delete article endpoints

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -2,6 +2,12 @@ const db = require("../models");
 const Article = db.Article;
 const { successMessage, status, errorMessage } = require("../helpers/status");
 
+/**
+ * Create Article
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} reflection object
+ */
 const createArticle = async (req, res) => {
   const { title, content, userId } = req.body;
   try {
@@ -20,6 +26,72 @@ const createArticle = async (req, res) => {
   }
 };
 
+/**
+ * Get all articles
+ * @param {object} req
+ * @param {object} res
+ * @returns {array} Articles array
+ */
+const getAllArticles = async (req, res) => {};
+/**
+ * Get article by id
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} article object
+ */
+const getArticleById = async (req, res) => {};
+
+/**
+ * Update Article
+ * @param {object} req
+ * @param {object} res
+ * @returns {void} article updated
+ */
+const updateArticle = async (req, res) => {
+  const { id } = req.params;
+  const { title, content, userId } = req.body;
+  try {
+    const updatedArticle = await Article.update(
+      { title, content, userId },
+      { where: { id } }
+    );
+    if (!updatedArticle[0]) {
+      errorMessage.error = "Article does not exist";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = "Article updated";
+    return res.status(status.success).send(successMessage);
+  } catch (error) {
+    errorMessage.error = `Article could not be updated: ${error.message}`;
+    return res.status(status.error).send(errorMessage);
+  }
+};
+
+/**
+ * Delete Article
+ * @param {object} req
+ * @param {object} res
+ * @returns {void} article deleted
+ */
+const deleteArticle = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const dbResponse = await Article.destroy({ where: { id } });
+    if (!dbResponse) {
+      errorMessage.error = "Article does not exist";
+      return res.status(status.notfound).send(errorMessage);
+    }
+    successMessage.data = "Article deleted";
+    return res.status(status.success).send(successMessage);
+  } catch (error) {
+    errorMessage.error = `Article could not be deleted: ${error.message}`;
+    return res.status(status.error).send(errorMessage);
+  }
+};
 module.exports = {
-  createArticle
+  createArticle,
+  getAllArticles,
+  getArticleById,
+  updateArticle,
+  deleteArticle
 };
