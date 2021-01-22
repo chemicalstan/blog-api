d25841f64279ecca91726fa1f1d478676fec2c48
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-22 17:15:52 +0100

    Describe test: truncate db and seed with a user

--- test/users.js
+++ test/users.js
@@ -1,21 +1,17 @@
-require('dotenv').config();
-process.env.NODE_ENV = 'test'; // set environment to test
-const mocha = require('mocha');
 const chai = require('chai');
-const chaiHttp = require('chai');
-const index = require('../index');
-const db = require('../models');
-const { User, sequelize } = db;
-// assertion style
-chai.should();
+const {assert} = chai;
+const truncate = require('./truncate');
+const userFactory = require('./factories/userFactory');
 
-chai.use(chaiHttp);
-// Mocha test description
-describe('Users API', ()=>{
-    beforeEach(async   (done)=>{ // Empty database before each test
-        await sequelize.query('TRUNCATE table "SequelizeMeta"');
-        await sequelize.drop({force: true});
-        await migrate()
-        done()
-    })
+describe('User model', () => {
+  let user;
+  beforeEach(async () => {
+    // Empties the test database
+    await truncate();
+    // creates a user
+    user = await userFactory();
+  });
+it('should do something', async () => {
+    // TODO
+  });
 });
