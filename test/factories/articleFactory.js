494e24ce5bfe87a50863c297ec041db31a6712a1
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-22 17:13:25 +0100

    Generate fake article instance

---
+++ test/factories/articleFactory.js
@@ -0,0 +1,25 @@
+const faker = require('faker');
+const {Article} = require('../../models');
+
+/**
+ * Generate an object which contains attributes needed
+ * to successfully create a article instance.
+ * 
+ * @param {object} props Properties to use for article
+ * @returns {} an object to build an article from
+ */
+const data = async (props={})=>{
+    const defaultProps= {
+        title: faker.lorem.sentence(),
+        content: faker.lorem.sentences(),
+        userId: 1
+    };
+    return Object.assign({}, defaultProps, props)
+}
+/**
+ * Generates an article instance from the properties provided.
+ * 
+ * @param {object} props properties to use for article
+ * @returns {} a user instance
+ */
+module.exports = async (props={})=>{ Article.create(await data(props))};
